2ca8a9888ccf3baeada9c206c05ac3a5e514afd3
David Kassa <david.kassa@gmail.com>
2021-10-24 18:25:13 -0500

    implement firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { doc, getFirestore } from 'firebase/firestore';
+import { FirestoreProvider, useFirestoreDocData, useFirestore, useFirebaseApp } from 'reactfire';
 
 function App() {
+  const firestoreInstance = getFirestore(useFirebaseApp());
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FirestoreProvider sdk={firestoreInstance}>
+      <h1>🌯</h1>
+      <BurritoTaste />
+    </FirestoreProvider>
   );
+}
+
+function BurritoTaste() {
+  // easily access the Firestore library
+  const burritoRef = doc(useFirestore(), 'tryreactfire', 'burrito');
+
+  // subscribe to a document for realtime updates. just one line!
+  const { status, data } = useFirestoreDocData(burritoRef);
+
+  // easily check the loading status
+  if (status === 'loading') {
+    return <p>Fetching burrito flavor...</p>;
+  }
+
+  return <p>The burrito is {data.yummy ? 'good' : 'bad'}!</p>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { doc, getFirestore } from 'firebase/firestore';
+import { FirebaseAppProvider, FirestoreProvider, useFirestoreDocData, useFirestore, useFirebaseApp } from 'reactfire';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBeD4MsNT2SeDyW8yzGtM0TC_gqFgxduL0",
+  authDomain: "unlucky-validators-dev.firebaseapp.com",
+  projectId: "unlucky-validators-dev",
+  storageBucket: "unlucky-validators-dev.appspot.com",
+  messagingSenderId: "1079518526397",
+  appId: "1:1079518526397:web:ea2bac245e1e090edfb0d4"
+};
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <App />
+    </FirebaseAppProvider>  
   </React.StrictMode>,
   document.getElementById('root')
 );
