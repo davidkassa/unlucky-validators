6f12a29ebcf2fbc17274691c8896436befccf1b0
David Kassa <david.kassa@gmail.com>
2021-10-24 20:09:27 -0500

    add environment config

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebaseConfig from './firebase';
 import reportWebVitals from './reportWebVitals';
 import { doc, getFirestore } from 'firebase/firestore';
 import { FirebaseAppProvider, FirestoreProvider, useFirestoreDocData, useFirestore, useFirebaseApp } from 'reactfire';
@@ -10,17 +11,6 @@ import { FirebaseAppProvider, FirestoreProvider, useFirestoreDocData, useFiresto
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBeD4MsNT2SeDyW8yzGtM0TC_gqFgxduL0",
-  authDomain: "unlucky-validators-dev.firebaseapp.com",
-  projectId: "unlucky-validators-dev",
-  storageBucket: "unlucky-validators-dev.appspot.com",
-  messagingSenderId: "1079518526397",
-  appId: "1:1079518526397:web:ea2bac245e1e090edfb0d4"
-};
-
 
 ReactDOM.render(
   <React.StrictMode>
@@ -34,4 +24,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
